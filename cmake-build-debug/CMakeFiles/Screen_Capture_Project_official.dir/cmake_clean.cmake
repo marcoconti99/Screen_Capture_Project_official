9dee8f4d18195f32d8d68051419a38b9cae5758d
Marco <s287854@studenti.polito.it>
2021-12-17 11:38:43 +0100

    lib ffmpeg

---
+++ cmake-build-debug/CMakeFiles/Screen_Capture_Project_official.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Screen_Capture_Project_official.dir/src/ScreenRecorder.cpp.o"
+  "CMakeFiles/Screen_Capture_Project_official.dir/src/main.cpp.o"
+  "Screen_Capture_Project_official.exe"
+  "Screen_Capture_Project_official.pdb"
+  "libScreen_Capture_Project_official.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Screen_Capture_Project_official.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
